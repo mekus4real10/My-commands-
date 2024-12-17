eb38dd90738fcbd6cf5c5837d13e7326afff760a
mekus4real10 <mosemeka09@gmail.com>
2024-12-16 21:28:30 -0600

    Shawplug

--- myWorkWithDafe/MyFirstScript.sh
+++ myWorkWithDafe/MyFirstScript.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "hello from my script"
+day="night"
+
+echo "From that $day on, Amos became $day not just as a lover of food, but as a $day of it. His $day had just begun, but one $day was certain: he would always respect the $day of a good meal"
+
+echo "hello from my script" > "$HOME/emekaAndI/automated-script.txt"
+
+ls .
+
+pwd
+
+
